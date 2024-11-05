1a3913dd08e7c85dc367abf8dc3e6ae489dc913b
Vinicius <vinicius.padua@tarvos.ag>
2024-11-05 20:21:30 -0300

    feat: add token in express middleware

--- node-api/src/main/adapters/express-middleware-adapter.ts
+++ node-api/src/main/adapters/express-middleware-adapter.ts
@@ -1,21 +1,23 @@
-import { Middleware } from '@/presentation/protocols'
+import { Middleware } from "@/presentation/protocols";
 
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from "express";
 
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
+    const tokenWithBearer = req.headers.authorization;
+    const token = tokenWithBearer?.split(" ")[1];
     const request = {
-      accessToken: req.headers?.['x-access-token'],
-      ...(req.headers || {})
-    }
-    const httpResponse = await middleware.handle(request)
+      accessToken: token,
+      ...(req.headers || {}),
+    };
+    const httpResponse = await middleware.handle(request);
     if (httpResponse.statusCode === 200) {
-      Object.assign(req, httpResponse.body)
-      next()
+      Object.assign(req, httpResponse.body);
+      next();
     } else {
       res.status(httpResponse.statusCode).json({
-        error: httpResponse.body.message
-      })
+        error: httpResponse.body.message,
+      });
     }
-  }
-}
+  };
+};
