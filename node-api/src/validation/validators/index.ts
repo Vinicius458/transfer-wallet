0d84b98c161d9118abd58b0e8cf8c47d32fde0b6
Vinicius <vinicius.padua@tarvos.ag>
2024-11-05 20:29:52 -0300

    feat: create new validation patterns

--- node-api/src/validation/validators/index.ts
+++ node-api/src/validation/validators/index.ts
@@ -2,3 +2,5 @@ export * from "./required-field-validation";
 export * from "./transaction-type-validation";
 export * from "./value-field-validation";
 export * from "./validation-composite";
+export * from "./compare-fields-validation";
+export * from "./email-validation";
