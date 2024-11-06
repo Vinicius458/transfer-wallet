d2268eb25068887e237bb39dc72df7cddd3ece2d
Vinicius <vinicius.padua@tarvos.ag>
2024-11-05 22:01:23 -0300

    feat: infra cryptography

--- node-api/src/presentation/controllers/index.ts
+++ node-api/src/presentation/controllers/index.ts
@@ -2,3 +2,4 @@ export * from "./transaction-controller";
 export * from "./add-accout-controller";
 export * from "./list-accounts-controller";
 export * from "./list-transactions-cotroller";
+export * from "./signup-controller";
