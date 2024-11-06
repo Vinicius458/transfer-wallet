369fc0cb027da17a6899ff57e04c2285db4f5c60
Vinicius <vinicius.padua@tarvos.ag>
2024-11-06 20:42:00 -0300

    feat: create config env

--- node-api/src/main/config/env.ts
+++ node-api/src/main/config/env.ts
@@ -1,9 +1,10 @@
 export default {
-  dbHost: process.env.DB_HOST || "mysql",
+  dbHost: process.env.DB_HOST || "localhost",
   dbPort: 3306,
   dbUser: process.env.DB_USER || "root",
   dbPassword: process.env.DB_PASSWORD || "123",
   dbName: process.env.DB_NAME || "bank",
+  jwtSecret: process.env.JWT_SECRET || "tj67O==5H",
   rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost",
   port: process.env.PORT || 3000,
 };
